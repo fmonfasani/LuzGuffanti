431ae758d740ea9fede43b81448627f662047565
Federico Monfasani <fmonfasani@gmail.com>
2026-01-25 19:49:27 -0300

    fix: hardcode cloudinary cloud name to resolve vercel build error

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "ddc5jpwq7",
+  },
 };
 
 export default nextConfig;
